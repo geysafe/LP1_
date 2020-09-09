692f5cd7528c4612ecf75ac257b2aee8c76172d1
Geysa Santos <fernanda.geysa@gmail.com>
2020-09-08 23:57:13 -0300

    Upload de atividade

---
+++ src/classes/Pessoa.java
@@ -0,0 +1,48 @@
+package classes;
+
+public class Pessoa {
+	private String nome;
+    private String sexo;
+    private int idade;
+    private float altura;
+    private String profissao;
+    
+    
+  //construtor
+    
+        
+    
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+    // metodos de classe: andar, caminhar, trabalhar
+
+}
---
+++ src/classes/TesteCalcado.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class TesteCalcado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
